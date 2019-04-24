36127d2df9d4ca6b98042702ca17d1df6f0a12c2
Carlos Javier Pendola <carlospendola@Carloss-MacBook-Pro.local>
2019-04-24 16:26:57 -0600

    first commit

---
+++ iJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iJournal
+//
+//  Created by Carlos Javier Pendola on 4/24/19.
+//  Copyright © 2019 Carlos Javier Pendola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
